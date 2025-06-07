a4fdc91b208eff53d48b79cf2017215712a85171
Lisseth Puco <le.pucoc@uea.edu.ec>
2025-06-06 20:27:46 -0500

    Update Encapsulacion.py

---
+++ encapsulacion/Encapsulacion.py
@@ -0,0 +1,26 @@
+class CuentaBancaria:
+    def __init__(self,saldo_inicial):
+        self.__saldo = saldo_inicial # atributo "privado"
+
+
+    def depositar(self, cantidad):
+        if cantidad > 0:
+            self.__saldo += cantidad 
+
+
+    def retirar(self, cantidad):
+        if 0 < cantidad <= self.__saldo:
+            self.__saldo -= cantidad 
+
+
+    def obtener_saldo(self):
+        return self.__saldo
+
+# Uso de la clase
+
+cuenta = CuentaBancaria(100)
+cuenta.depositar(50)
+cuenta.retirar(30)
+
+
+print("Saldo actual:", cuenta.obtener_saldo())
